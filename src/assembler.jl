c6931ef8cdbed9ef64e2139544a2f19997ae440a
Jorge Pérez Zerpa <42485529+jorgepz@users.noreply.github.com>
2022-03-07 13:10:58 -0300

    Update assembler.jl

--- src/assembler.jl
+++ src/assembler.jl
@@ -4,6 +4,8 @@ function assembler( nodalCoords, elemNodalConnec, elemMEBIVals, materialsData, e
     nElems     = size( elemNodalConnec, 1) ;
     nNodes     = size( nodalCoords,     1) ;
 
-    return 1;
+    
+    
+    return Matrix, vector;
 
 end
